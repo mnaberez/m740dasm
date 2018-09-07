98034693367433125c621be6945fa089bf2dcbae
Mike Naberezny <mike@naberezny.com>
2018-09-07 14:41:48 -0700

    Remove entry points specific to VW Gamma 5 firmware

--- m740dasm/command.py
+++ m740dasm/command.py
@@ -13,11 +13,7 @@ def main():
 
     memory = Memory(rom)
 
-    entry_points = [0xc2eb,0x5a11,0x33e0,0x33ee,0x33f9,0x3405,0x3429,0x3435,0x3447,0x3458,
-                    0xc31f,0xce8d,0xc9c3,0xc3e4,0xc40f,0xc446,0xc47e,0xc2c4,0xc4dd,0xc540,
-                    0xc581,0xc5a1,0x5ab5,0x5ad5,0x5adf,0x5b16,0x5b1d,0x5b37,0x5b4a,0x5ce4,
-                    0x5bbb,0x345c,0x3463,0x3466,0x3477,0x34a8,0x34b3,0x34d6,0x34d9,0x34e5,
-                    0x7f7f,]
+    entry_points = []
 
     vectors = [
         # brk
@@ -28,20 +24,6 @@ def main():
         # reset
         0xfffc,
     ]
-
-    # 3570 - 3587 table of bytes for comparison
-    for i in range(0x3588, 0x35b8, 2):
-        vectors.append(i)
-
-    for i in range(0x351e, 0x3570, 2):
-        vectors.append(i)
-
-    for i in range(0xb4db, 0xb50d, 2):
-        vectors.append(i)
-
-    # group reading handlers
-    for i in range(0xa53a, 0xa54e, 2):
-        vectors.append(i)
 
     traceable_range = range(start_address, start_address + len(rom) + 1)
     tracer = Tracer(memory, entry_points, vectors, traceable_range)
